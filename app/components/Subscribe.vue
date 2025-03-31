bd03f01c2460bb10c97ea64bec91eedf6dc5c2f3
lo0v777@mail.ru <lo0v777@mail.ru>
2025-03-31 21:35:51 +0300

    new

--- app/components/Subscribe.vue
+++ app/components/Subscribe.vue
@@ -1,7 +1,51 @@
 <template>
-    <h1>THIS DOCUMENT</h1>
+    <div class="button-container">
+      <el-button type="warning" class="back">Назад</el-button>
+      <el-button type="info" class="down">
+        <div class="flex flex-col items-center">
+            <el-icon>
+                <ArrowDownBold />
+            </el-icon>
+        </div>
+        
+        
+
+      </el-button>
+      <el-button type="success" class="subscribe">Подписать</el-button>
+    </div>
+    
 </template>
+  
+<script lang="ts" setup>
 
-<script setup lang="ts">
-
+import { ArrowDownBold } from '@element-plus/icons-vue'
+import pdfFile from '~/assets/image/1.pdf'
 </script>
+  
+<style scoped lang="scss">
+.button-container {
+    display: flex;
+    flex-direction: column; 
+    height: 80vh; 
+    padding: 25px;
+    justify-content: space-between;
+}
+  
+.subscribe {
+    margin-top: 30px;
+    width: 150px; 
+    height: 40px; 
+
+}
+  
+.down {
+    margin-left: auto; 
+    margin-top: -480px; 
+}
+  
+.back {
+    margin-right: auto; 
+    margin-top: 10px; 
+}
+</style>
+  
