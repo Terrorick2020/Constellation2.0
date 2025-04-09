630b195d77253fbba7d02987365ca0922b573e93
unknown <Rick-Sanches2020@yandex.ru>
2025-04-09 12:12:44 +0300

    исправление ошибки при билде

--- app/server/routes/_ws.ts
+++
@@ -1,9 +0,0 @@
-import type { Peer, Message } from 'crossws'
-
-export default defineWebSocketHandler({
-  open(peer: Peer) {},
-
-  message(peer: Peer, message: Message) {},
-
-  close(peer: Peer) {}
-})
