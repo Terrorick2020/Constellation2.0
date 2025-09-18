199ad6971c93e4bf375efa673e90e3dab64c2ea1
ta1kin <ilataraskin475@gmail.com>
2025-09-18 09:32:35 +0300

    fix chat

--- api/src/chats/chats.module.ts
+++ api/src/chats/chats.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common'
+import { JwtStrategy } from '../auth/jwt.strategy'
 import { PrismaModule } from '../prisma/prisma.module'
 import { RedisModule } from '../redis/redis.module'
 import { StorageService } from '../storage/storage.service'
@@ -9,7 +10,7 @@ import { ChatsService } from './chats.service'
 @Module({
 	imports: [PrismaModule, RedisModule],
 	controllers: [ChatsController],
-	providers: [ChatsService, ChatsGateway, StorageService],
+	providers: [ChatsService, ChatsGateway, StorageService, JwtStrategy],
 	exports: [ChatsService]
 })
 export class ChatsModule {}
