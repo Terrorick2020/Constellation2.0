d0265a475e5f0525847547741b9e065b67bc81d1
lo0v777@mail.ru <lo0v777@mail.ru>
2025-09-19 14:39:29 +0300

    new

--- api/wait-for-db.sh
+++ api/wait-for-db.sh
@@ -1,14 +1,14 @@
-# #!/bin/bash
+#!/bin/bash
 
-# # Ждем, пока база данных будет доступна на порту 5432
-# until nc -z -v -w30 db 5432; do
-#   echo "Waiting for database connection..."
-#   sleep 1
-# done
+# Ждем, пока база данных будет доступна на порту 5432
+until nc -z -v -w30 db 5432; do
+  echo "Waiting for database connection..."
+  sleep 1
+done
 
-# # Когда база данных доступна, выполняем миграции и запускаем приложение
-# echo "Database is up, running prisma db push"
-# npx prisma db push
+# Когда база данных доступна, выполняем миграции и запускаем приложение
+echo "Database is up, running prisma db push"
+npx prisma db push
 
-# # Запускаем приложение
-# npm run start
+# Запускаем приложение
+npm run start
