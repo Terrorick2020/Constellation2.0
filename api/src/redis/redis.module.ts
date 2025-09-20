4b7b0969418624b0cf0e1eee3e159da19808bcc2
ta1kin <ilataraskin475@gmail.com>
2025-09-20 11:25:36 +0300

    add ws

--- api/src/redis/redis.module.ts
+++ api/src/redis/redis.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common'
+import { ConfigModule } from '@nestjs/config'
 import { RedisPubSubService } from './redis-pub-sub.service'
 import { RedisService } from './redis.service'
 
 @Module({
+  imports: [ConfigModule],
   providers: [RedisService, RedisPubSubService],
   exports: [RedisService, RedisPubSubService],
 })
